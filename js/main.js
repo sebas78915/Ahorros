6e9c7756eb19fc1aacbbff24c0b73697b33b987a
Andguja <andguja@gmail.com>
2019-05-11 06:01:00 -0500

    Nuevo ahorro +navegacion
    la pantalla nuevo ahorro y su respectiva navegacion.

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ var pantalladecarga;
 var pantalladelogin;
 var continuarlogin;
 var pantalla_ahorros;
+var botonnuevoahorro;
+var pantallanuevoahorro;
+var boton_cancelarnuevoahorro;
 function inicio(){
     inicializarvariables();
     main();
@@ -15,15 +18,25 @@ function inicializarvariables()
         pantalladelogin=document.getElementById("inicio");
         pantalladecarga=document.getElementById("splash-screen");
         continuarlogin=document.getElementById("continuar-login");
+        botonnuevoahorro=document.getElementById("nuevo_ahorro");
         pantalla_ahorros=document.getElementById("mis_ahorros");
+        pantallanuevoahorro=document.getElementById("pantallanuevoahorro");
+        boton_cancelarnuevoahorro=document.getElementById("salir_nuevoahorro");
         continuarlogin.addEventListener("click",continuelogin); 
+        botonnuevoahorro.addEventListener("click",new_ahorro);
+        boton_cancelarnuevoahorro.addEventListener("click",salir_nuevoahorro)
 
     }
 function main(){
     mostrarpantalla(pantalladecarga);
     setTimeout("cambiopantalla(pantalladelogin,pantalladecarga)", 1500);
     }
-
+    function salir_nuevoahorro(){
+        cambiopantalla(pantalla_ahorros,pantallanuevoahorro);
+    }
+function new_ahorro(){
+    cambiopantalla(pantallanuevoahorro,pantalla_ahorros);
+}
     function mostrarpantalla(pantalla){
         pantalla.className=pantalla.className.replace( /(?:^|\s)ocultar(?!\S)/g , '' );
       }
